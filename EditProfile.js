b3fc0db31359f4261be0e3006df6d223167b2c47
Aravind Krishna <62590521+aravinkrishna@users.noreply.github.com>
2020-06-01 19:31:36 +0530

    bug fix in viewProfile

---
+++ EditProfile.js
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import axios from 'axios'
+
+
+class EditProfile extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             userName: this.props.userName,
+             name: this.props.name,
+             bio: "",
+             update: {
+                 bio: "",
+                 fullName: "",
+                 profilePic: null
+             },
+            successMessage: "",
+            errorMessage: "",
+            uploadStatus: ""
+        }
+        
+        this.getUserDetails();
+    }
+
+    getUserDetails = () => {
+        
+        axios.get("http://localhost:1050/fetchUserDet/"+this.state.userName).then(res=>{
+            
+            this.setState({
+                bio: res.data.bio,
+                name: res.data.fullName
+            })
+        })
+
+    }
+    
+    handleChange = (event) => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({
+            update: {
+                ...this.state.update,[name]: value
+            }
+        })
+    }
+
+    updateBio = () => {
+        let bioObj = {
+            bio: this.state.update.bio
+        }
+        axios.put('http://localhost:1050/editBio/'+this.state.userName,bioObj).then(res=>{
+            
+            this.setState({
+                successMessage: res.data,
+                errorMessage: ""
+            })
+        }).catch(err=>{
+            this.setState({
+                errorMessage: "Failed to update",
+                successMessage: ""
+            })
+        })
+    }
+
+    updatefullName = () => {
+        let fullNameObj = {
+            fullName: this.state.update.fullName
+        }
+        axios.put('http://localhost:1050/editFullName/'+this.state.userName,fullNameObj).then(res=>{
+        
+            this.setState({
+                successMessage: res.data,
+                errorMessage: ""
+            })
+            this.props.setName(this.state.update.fullName,this.state.userName)
+        }).catch(err=>{
+            this.setState({
+                errorMessage: "Failed to update",
+                successMessage: ""
+            })
+        })
+
+    }
+
+    profilePicSelected = ( event ) => {
+        this.setState({
+            update: {
+                ...this.state.update,["profilePic"]: event.target.files[0] 
+            }
+        })
+    }
+
+    uploadProfilePic = () => {
+        let url = 'http://localhost:1050/editProfilePic/'+this.state.userName;
+        const fd = new FormData();
+        fd.append('profilePic', this.state.update.profilePic, this.state.update.profilePic.name);
+        axios.put(url, fd,{
+            onUploadProgress: ProgressEvent => {
+                    let val = Math.round(ProgressEvent.loaded / ProgressEvent.total * 100)
+                    if(val<100){
+                        this.setState({
+                            uploadStatus: "uploading..."
+                        })
+                    }
+                }
+        }).then(res=>{
+            // console.log(res.data)
+            this.setState({
+                successMessage: res.data,
+                errorMessage: "",
+                uploadStatus: ""
+            })
+        }).catch(err=>{
+            this.setState({
+                errorMessage: "Update failed!",
+                successMessage: "",
+                uploadStatus: ""
+            })
+        })
+    }
+    
+    render() {
+        return (
+            <div className="container-fluid mt-3">
+                <div>
+                    <Link to="/dashboard">
+                        <button className="btn btn-danger">
+                            back to dashboard
+                        </button>
+                    </Link>
+                </div>
+                <div className="row">
+                    <div className="col-md-9 col-sm-12 offset-md-1">
+                        {this.state.successMessage !== ""?
+                            <div className="alert alert-success">{this.state.successMessage}</div>
+                            : this.state.errorMessage !== ""?
+                                <div className="alert alert-danger">{this.state.errorMessage}</div>
+                                :
+                                null
+                        }
+                        <div className="card mt-2">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-3">
+                                        <span>Select profile picture</span>
+                                    </div>
+                                    <div className="col-6">
+                                        <input type="file" onChange={this.profilePicSelected}/>
+                                        {this.state.uploadStatus}
+                                    </div>
+                                    <div className="col-3">
+                                        <button className="btn btn-primary"
+                                        onClick={this.uploadProfilePic}
+                                        disabled={this.state.update.profilePic===null ? true: false}>
+                                            Upload image
+                                        </button>
+                                    </div>
+                                </div>
+                                <br />
+                                <div className="row">
+                                    <div className="col-3">
+                                        <span>enter full name</span>
+                                    </div>
+                                    <div className="col-6">
+                                        <input type="text" className="form-control" 
+                                        name = "fullName"
+                                        onChange={this.handleChange}
+                                        placeholder={this.state.name}/>
+                                    </div>
+                                    <div className="col-3">
+                                        <button className="btn btn-primary" 
+                                        onClick={this.updatefullName}
+                                        disabled={this.state.update.fullName === ""?true:false}>
+                                            Update name
+                                        </button>
+                                    </div>
+                                </div>
+                                <br />
+                                <div className="row">
+                                    <div className="col-3">
+                                        <span>enter bio</span>
+                                    </div>
+                                    <div className="col-6">
+                                        <input type="text" className="form-control" 
+                                        name = "bio"
+                                        onChange={this.handleChange}
+                                        placeholder={this.state.bio}/>
+                                    </div>
+                                    <div className="col-3">
+                                        <button className="btn btn-primary" onClick={this.updateBio}
+                                         disabled={this.state.update.bio === ""?true:false}>
+                                            Update bio
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default EditProfile
---
+++ SearchUsers.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+
+class SearchUsers extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             userName: this.props.userName,
+             users: [],
+             followedUsers: [],
+             userDet: null,
+             successMessage: "",
+             errorMessage: "",
+        }
+    this.getAllUsers();
+    }
+
+getAllUsers = () => {
+    axios.get('http://localhost:1050/fetchUserDet/'+this.state.userName).then(res=>{
+        this.setState({
+            userDet: res.data
+        })
+    }).catch(err=>{
+        console.log(err)
+        // this.setState({
+        //     errorMessage: err.response.data
+        // })
+    })
+
+    axios.get('http://localhost:1050/fetchAllUserNames').then(res=>{
+        this.setState({
+            users: res.data
+        },()=>this.getProfileImages())
+    }).catch(err=>{
+        console.log(err)
+        // this.setState({
+        //     errorMessage: err.response.data
+        // })
+    })
+}
+
+getProfileImages = () => {
+    const arrImg = [];
+    this.state.users.forEach(( user )=>{
+        if(user.profilePic){
+            let url = 'http://localhost:1050/'+user.profilePic
+            axios.get(url, { responseType: 'arraybuffer'} ).then(res=>{
+                let base64 = btoa(
+                    new Uint8Array(res.data).reduce(
+                    (data, byte) => data + String.fromCharCode(byte),
+                    '',
+                ),
+                );
+            let obj = {
+                userName: user.userName,
+                fullName: user.fullName,
+                profilePic: "data:;base64," + base64
+                }
+            arrImg.push(obj);
+            this.setState({
+                users: arrImg
+            })
+            }).catch(err=>{
+                this.setState({
+                    errorMessage: err.response.data
+
+                })
+            })
+        } else {
+            arrImg.push(user);
+            this.setState({
+                users: arrImg
+            })
+        }
+    })
+}
+
+followUser = ( uNameToFollow ) => {
+    let url = 'http://localhost:1050/followUser/'+this.state.userName+'/'+uNameToFollow;
+    axios.put(url,{}).then(res=>{
+        this.setState({
+            successMessage: res.data
+        })
+        let arr = this.state.followedUsers
+        arr.push(uNameToFollow)
+        this.setState({
+            followedUsers: arr
+        })
+    }).catch(err=>{
+        console.log(err)
+        // this.setState({
+        //     errorMessage: "Error occured during following try again later :("
+        // })
+    })
+}
+
+search = ( event ) => {
+    
+    let value = event.target.value;
+    let userArr = [];
+    userArr = this.state.users.filter( user => {
+        // console.log(user)
+        if(user.fullName.toUpperCase().match(value.toUpperCase())){
+            return true
+        }
+    })
+    // console.log(userArr);
+    this.setState({
+        users: userArr
+    })
+    if(value === "") {
+        this.getAllUsers();
+    }
+}
+    
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="mt-3">
+                    <Link to="/dashboard">
+                        <button className="btn btn-danger">
+                            back to dashboard
+                        </button>
+                    </Link>
+                </div>
+                <div className="row mt-2">
+                    <div className="col-md-6 offset-md-3 col-sm-12">
+                    {this.state.successMessage !== ""?
+                            <div className="alert alert-success">{this.state.successMessage}</div>
+                            : this.state.errorMessage !== ""?
+                                <div className="alert alert-danger">{this.state.errorMessage}</div>
+                                :
+                                null
+                    }
+                        <div className="card">
+                            <div className="card-header">
+                                Seach for users
+                            </div>
+                            <div className="card-body">
+                                <input type = "text" name = "search" className="form-control"
+                                placeholder = "Enter name to search" 
+                                onChange={this.search}/>
+                            </div>
+                        </div>
+                        <br/>
+                        {this.state.users.length > 0 && this.state.userDet?
+                            this.state.users.map((user)=>
+                                this.state.userName != user.userName && user.userName != "Dummy_user"?
+                                        <Link to={ { pathname: '/viewProfile', state: { viewerUserName: this.state.userName, userName: user.userName } }}>
+                                            <div>
+                                                <div className="card">
+                                                <div className="card-body row">
+                                                    <div className="col-3 search-img">
+                                                        {user.profilePic?
+                                                        <img src={user.profilePic} />
+                                                        :
+                                                        <img src={require('./Assets/no_img.JPG')} alt="no_img" />
+
+                                                        }
+                                                    </div>
+                                                    <div className="col-6">
+                                                        <span className="h5">{user.fullName}</span>
+                                                        <br/>
+                                                        <span className="text-dark">@{user.userName}</span>
+                                                    </div>
+                                                    <div className="col-3">
+                                                        <button className="btn btn-info float-right" 
+                                                            onClick={()=>this.followUser(user.userName)}
+                                                            disabled = {this.state.userDet.following.includes(user.userName) ||
+                                                            this.state.followedUsers.includes(user.userName)} >
+                                                                {this.state.userDet.following.includes(user.userName) ||
+                                                                    this.state.followedUsers.includes(user.userName)?
+                                                                    <span>following</span>
+                                                                    :
+                                                                    <span>follow</span>
+                                                                }
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                <br/>
+                                            </div>
+                                        </Link>
+                                        :
+                                        null
+                                
+                            )
+                            :
+                            <div className="alert alert-danger">No users to display :(</div>
+                            
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchUsers
